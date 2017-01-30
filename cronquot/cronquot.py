b3b2661b209f32c316c2c90f345d3e8747e928a9
pyohei <mukaishohei76@gmail.com>
2017-01-30 22:48:49 +0900

    Change to call iterable.

--- cronquot/cronquot.py
+++ cronquot/cronquot.py
@@ -21,62 +21,6 @@ def stop_future_warning(func):
             return func(*args, **kwargs)
     return wrapper
 
-
-def __create_file_object_by_server(directory):
-    """Read cronfile and create file object."""
-    server_list = {}
-    for server_file in os.listdir(directory):
-        file_path = os.path.join(directory, server_file)
-        with open(file_path, 'r') as f:
-            lines = f.readlines()
-            name, __ext = os.path.splitext(server_file)
-            server_list[name] = lines
-    return server_list
-
-
-@stop_future_warning
-def main(directory, start, end):
-    """Main process."""
-    targets = {}
-
-    for sv, lines in __create_file_object_by_server(directory).items():
-        for li in lines:
-            li = li.rstrip()
-            if not is_cron_script(li):
-                continue
-
-            nz_crnon = normalize_cron_script(li)
-            cron_time = nz_crnon[0]
-            cron_script = nz_crnon[1]
-
-            # TODO: has to handle error occuring missed cron script.
-            c = CronTab(cron_time)
-
-            current_time = start
-            while current_time <= end:
-                interval_second = c.next(current_time)
-                current_time = current_time + \
-                    timedelta(seconds=interval_second)
-                if current_time > end:
-                    break
-                cron_combo = (sv, cron_script)
-                if current_time not in targets:
-                    targets[current_time] = [cron_combo]
-                else:
-                    targets[current_time].append(cron_combo)
-    return targets
-
-def is_cron_script(cron_script):
-    for ic in IGNORE_CASES:
-        if ic in cron_script or cron_script == '':
-            return False
-    return True
-
-def normalize_cron_script(cron_script):
-    result = re.sub(r'  +', '\t', cron_script)
-    result_list = result.split('\t')
-    return (' '.join(result_list[0:5]), result_list[-1])
-
 def export(targets):
     header = 'date, hour, miniute, second, scrip, server\n'
     with open('result.csv', 'w') as f:
@@ -115,8 +59,10 @@ def parse_command():
     if not has_directory(directory):
         sys.exit(1)
     
-    t = main(directory, start, end)
-    export(t)
+    c = Cron(directory, start, end)
+    for cc in c:
+        print cc
+    # export(t.crons)
 
 def has_directory(directory):
     if os.path.isdir(directory):
@@ -126,6 +72,68 @@ You must make `crontab` directory to load crontab files.
 If you don't want this name, you can chose your prefere name with argument of
 `-d`. Please see help command.\n""")
     return False
+
+
+class Cron(object):
+
+    def __init__(self, directory, start, end):
+        self.crons = self.__quote(directory, start, end)
+
+    @stop_future_warning
+    def __quote(self, directory, start, end):
+        targets = {}
+        for sv, lines in self.create_file_object_by_server(directory).items():
+            for li in lines:
+                li = li.rstrip()
+                if not self.is_cron_script(li):
+                    continue
+
+                nz_crnon = self.normalize_cron_script(li)
+                cron_time = nz_crnon[0]
+                cron_script = nz_crnon[1]
+
+                # TODO: has to handle error occuring missed cron script.
+                c = CronTab(cron_time)
+
+                current_time = start
+                while current_time <= end:
+                    interval_second = c.next(current_time)
+                    current_time = current_time + \
+                        timedelta(seconds=interval_second)
+                    if current_time > end:
+                        break
+                    cron_combo = (sv, cron_script)
+                    if current_time not in targets:
+                        targets[current_time] = [cron_combo]
+                    else:
+                        targets[current_time].append(cron_combo)
+        return targets
+    
+    def __iter__(self):
+        for t, s in sorted(self.crons.items()):
+            yield (t, s)
+
+    def create_file_object_by_server(self, directory):
+        """Read cronfile and create file object."""
+        server_list = {}
+        for server_file in os.listdir(directory):
+            file_path = os.path.join(directory, server_file)
+            with open(file_path, 'r') as f:
+                lines = f.readlines()
+                name, __ext = os.path.splitext(server_file)
+                server_list[name] = lines
+        return server_list
+
+    def is_cron_script(self, cron_script):
+        for ic in IGNORE_CASES:
+            if ic in cron_script or cron_script == '':
+                return False
+        return True
+
+    def normalize_cron_script(self, cron_script):
+        result = re.sub(r'  +', '\t', cron_script)
+        result_list = result.split('\t')
+        return (' '.join(result_list[0:5]), result_list[-1])
 
 
 if __name__ == '__main__':
