3863917a423a391098e0c7f3f822454da4090b3c
pyohei <mukaishohei76@gmail.com>
2017-02-04 11:16:55 +0900

    Revert "Fix testing code."
    This reverts commit 61705d28c7e65d4fc6b3442df91b99ad883bd522.

--- test/test_cronquot.py
+++ test/test_cronquot.py
@@ -5,7 +5,7 @@ from cronquot.cronquot import has_directory
 class CronquotTest(unittest.TestCase):
 
     def test_has_directory(self):
-        self.assertTrue(has_directory('/tmp2'))
+        self.assertTrue(has_directory('/tmp'))
 
 if __name__ == '__main__':
     unittest.test()
