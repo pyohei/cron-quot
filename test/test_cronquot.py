13b6e289f3ced59068d91dff2b2ef12a7805fabe
pyohei <mukaishohei76@gmail.com>
2017-02-11 16:44:32 +0900

    Create test definitions.

--- test/test_cronquot.py
+++ test/test_cronquot.py
@@ -10,5 +10,20 @@ class CronquotTest(unittest.TestCase):
                 os.path.dirname(__file__), 'crontab')
         self.assertTrue(has_directory(sample_dir))
 
+    def test_parse_command(self):
+        pass
+
+    def test_is_cron_script(self):
+        pass
+
+    def test_normalize_cron_script(self):
+        pass
+
+    def test_has_cosistency_in_result(self):
+        pass
+
+    def test_simple_cron_pattern(self):
+        pass
+
 if __name__ == '__main__':
     unittest.test()
