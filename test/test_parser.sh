d6b62f618a7fff2fd4a3a8f3a8664120f2183e01
pyohei <mukaishohei76@gmail.com>
2017-03-18 15:05:11 +0900

    Fix test scripts.

--- test/test_parser.sh
+++ test/test_parser.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+# Check comparable file.
+DIFF_FILE="test/test_result.csv"
+if [ ! -e $DIFF_FILE ]; then
+    echo "No diff file($DIFF_FILE)."
+    exit 1
+fi
+
 # Black Box test for cron parser.
 rm result.csv
 
-python ../cronquot/cronquot.py -s 20161230200000 -e 20170104101111 -d crontab
+python ../cronquot/cronquot.py -s 20161230200000 -e 20170104010000 -d crontab
 
 RESULT=`diff result.csv test/test_result.csv`
 
