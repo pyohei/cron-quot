5cb05b7ae058c334737c44796313edc9593197a8
pyohei <mukaishohei76@gmail.com>
2018-06-06 20:30:15 +0900

    Fix setup settings.

--- setup.py
+++ setup.py
@@ -1,33 +1,29 @@
 """Set up script"""
 import codecs
 from setuptools import setup
-import pypandoc
 
 
 def _create_long_desc():
     """Create long description and README formatted with rst."""
     _long_desc = ''
-    with codecs.open('README.rst', 'w', 'utf-8') as rf:
-        try:
-            _long_desc = pypandoc.convert('README.md', 'rst')
-        except OSError as e:
-            import os
-            if not 'TRAVIS' in os.environ:
-                raise OSError(e)
-            else:
-                return ''
-        rf.write(_long_desc)
-    return _long_desc
+    if os.path.isfile('README.md'):
+        with open('README.md', 'r') as rf:
+            return rf.read()
+    if os.path.isfile('README.rst'):
+        with open('README.rst', 'r') as rf:
+            return rf.read()
 long_desc = _create_long_desc()
+
 
 # Setup
 setup(name='cronquot',
-      version='0.1.0',
+      version='0.1.1',
       description='Cron scheduler.',
       long_description=long_desc,
       classifiers=[
           'Programming Language :: Python',
           'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.6',
           'License :: OSI Approved :: MIT License',
           'Topic :: Software Development :: Libraries :: Python Modules'
           ],
